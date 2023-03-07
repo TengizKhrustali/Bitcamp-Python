7012410a2bff45720bdd0b560242891c763f736a
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-03-07 17:08:23 +0400

    Create Calculator (Square).py

---
+++ Calculator (Square).py
@@ -0,0 +1,13 @@
+# This program squares integers
+
+# Define main function
+def main():
+    x = int(input("What is x? "))
+    print("x squared is ", square(x))
+
+# Define square funtion
+def square(n):
+     return n * n  
+
+# Call main function
+main()

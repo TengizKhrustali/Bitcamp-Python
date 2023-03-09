cc9a9e669bf3e6e53b18fa9ecebf1cd43df3d34e
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-03-09 14:55:02 +0400

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,10 @@
+# This program prompts user to enter a text and returns text at a slower speed ;)
+
+text = str(input("Enter some text: "))
+modified_text = text.replace(' ', '...')
+print(modified_text)
+
+
+
+
+

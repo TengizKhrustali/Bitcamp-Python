b11eb2a6d2ddf4616b8f7210242493c673953b30
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-03-09 09:38:00 +0400

    Create check if even, odd, divides evenly or not.py

---
+++ check if even, odd, divides evenly or not.py
@@ -0,0 +1,17 @@
+# define input
+num = int(input("give me a number to check: "))
+check = int(input("give me a number to divide by: "))
+
+# check if number is odd, even or multiple of 4
+if num % 4 == 0:
+    print(num, "is a multiple of 4")
+elif num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
+
+# Check if number divides evenly or not
+if num % check == 0:
+    print(num, "divides evenly by", check)
+else:
+    print(num, "does not divide evenly by", check)

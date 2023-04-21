02789e24c49617b5f9bb0b2e4163eb8e230e30ae
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-04-21 12:36:17 +0400

    Update fuel.py

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,1 +1,27 @@
+def main():
+    
+    while True: # loops forever
+        # strips input and splits it into two numbers
+        fraction = (input("Fraction: ")).strip().split("/") 
+
+        try:
+            # takes two numbers, converts to int divides and multipies by 100% to get percentage
+            percentage = int(int(fraction[0])/int(fraction[1])*100)
+
+            if percentage > 100:
+                pass
+            elif percentage <= 1:
+                print("E")
+            elif percentage >=99:
+                print("F")
+            else:
+                print(percentage, "%", sep="")
+        
+        except ZeroDivisionError:
+            print("Cant devide by zero;")
+        except ValueError:
+            print("Invalid fraction;")
+    
+if __name__ == "__main__":
+    main()
 

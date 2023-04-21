73d23fc2a4bb4e19cf7395b8cafd592d763bef00
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-04-21 11:11:14 +0400

    Update nutrition.py

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,1 +1,13 @@
+def main():
+    x = input("please input fruit name: ").lower()
+    fruits_and_calories = { "apple":130, "avocado":50, "banana": 110, "cantaloupe":50,"grapefruit":60, 
+    "grapes":90, "kiwifruit":90, "lemon":15, "lime":20, "nectarine":60,
+    "orange":80, "peach":60, "pear":100, "pineapple":50,"plums":70, 
+    "sweet cherries" :100, "strawberries":50, "tangerine":50, "watermelon":8}
 
+    if x in fruits_and_calories:
+        print("Calories: " + str(fruits_and_calories[x]))
+
+
+if __name__ == "__main__":
+    main()

f116117b83e8708c3b31e2bd2257abad69d400f1
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-05-04 10:09:23 +0400

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,26 +1,18 @@
-"""
-In a file called interpreter.py, implement a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point 
-value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+def main():
+    # get user input
+    user_input = input("Expression: ")
+    # print result of interpreter function, which takes user input
+    print(interpreter(user_input))
 
-x is an integer
-y is +, -, *, or /
-z is an integer
-For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
-
-Note that, just as python itself is an interpreter for Python, so will your interpreter.py be an interpreter for math!
-"""
-
-expression = input("Expression: ")
-x, y, z = expression.split()
-new_x = float(x)
-new_z = float(z)
-
-if y == "+":
-    result = new_x + new_z
-elif y == "-":
-    result = new_x - new_z
-elif y == "*":
-    result = new_x * new_z
-elif y == "/":
-    result = new_x / new_z
-print(round(result, 1))
+def interpreter(str):
+    # create a list of operators
+    operators = ["+", "-", "/", "*"]
+    # create a loop where if variable "operator" is in operator, so user inputs one of the operators, split by operator.
+    for operator in operators:
+        if operator in str:
+            x, z = str.split(operator)
+            z = float(z)
+            x = float(x)
+            # convert string into operator
+            return eval(f"{x} {operator} {z}")
+main()

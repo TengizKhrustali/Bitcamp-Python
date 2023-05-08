281aa973bf2e701dd738fa98d4ad4a0c75008850
TengizKhrustali <105299123+TengizKhrustali@users.noreply.github.com>
2023-05-08 16:09:00 +0400

    Update test.py

--- twttr/test.py
+++ twttr/test.py
@@ -1,8 +1,20 @@
-import pytest
-from twttr import replace_vowels
+from twttr import shorten
 
+def main():
+    test_upper()
+    test_lower()
+    test_symbols()
 
-def test():
-    assert replace_vowels("twitter") == "twttr"
+def test_upper():
+    assert shorten("TEN") == "TN"
 
+def test_lower():
+    assert shorten("hello") == "hll"
+    assert shorten("world") == "wrld"
+    assert shorten("aeiou") == ""
 
+def test_symbols():
+    assert shorten("HELLO$$") == "HLL$$"
+
+if __name__ == "__main__":
+    main()
